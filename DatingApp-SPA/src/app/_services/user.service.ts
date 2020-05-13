e3a52c3c8c4c87818995b8c3c50f954804db5d25
Mohan Rathod <mohan.rathod@evry.com>
2020-05-13 15:46:03 +0530

    Before End OF section 10

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -17,8 +17,12 @@ getUsers(): Observable<User[]> {
   return this.http.get<User[]>(this.baseUrl + 'users');
 }
 
-getUser(id): Observable<User> {
+getUser(id: number): Observable<User> {
   return this.http.get<User>(this.baseUrl + 'users/' + id);
+}
+
+updateUser(id: number, user: User) {
+  return this.http.put(this.baseUrl + 'users/' + id, user);
 }
 
 }
